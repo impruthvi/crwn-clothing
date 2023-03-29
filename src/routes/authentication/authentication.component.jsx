23e4db7f18b5323c416bda1e59ae684406cb1a97
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-03-29 15:36:32 +0530

    implemented a sign In With Email And Password

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,18 +1,11 @@
+import SignInForm from '../../components/sign-in-foem/sign-in-form.component';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from '../../utils/firebase/firebase.util';
+
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef);
-  };
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
