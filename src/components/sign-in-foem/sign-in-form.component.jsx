277f611be559be4dae4cdd32ed21bbe68a5c637b
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-03-29 17:15:56 +0530

    created a user context and set the value and access the value from that

--- src/components/sign-in-foem/sign-in-form.component.jsx
+++ src/components/sign-in-foem/sign-in-form.component.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+
 import {
   createUserDocumentFromAuth,
   signInAuthWithEmailAndPassword,
@@ -6,6 +7,8 @@ import {
 } from '../../utils/firebase/firebase.util';
 import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
+import { UserContext } from '../../context/user.context'
+
 import './sign-in.styles.scss';
 
 const defaultFormFields = {
@@ -16,6 +19,8 @@ const defaultFormFields = {
 const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext)
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -30,8 +35,8 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthWithEmailAndPassword(email, password);
-      console.log(response);
+      const { user } = await signInAuthWithEmailAndPassword(email, password);
+      setCurrentUser(user)
       resetFormFields();
     } catch (error) {
       switch (error.code) {
