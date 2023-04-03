1ca41dad99d6845c831ffb20f22c99f4864f58e9
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-03 10:02:27 +0530

    Styled the Product cart

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,14 @@
 import { useContext } from 'react';
 import { ProductsContext } from '../../context/product.context'
+import ProductCart from '../../components/product-card/product-card.component';
+import './shop.styles.scss';
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   
-  return <div>
-    {products.map(({ id, name }) => {
+  return <div className='products-container'>
+    {products.map((product) => {
       return (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+        <ProductCart key={product.id} product={product}/>
       );
     })}
   </div>;
