00d18d1ff23bb888c421ac00c9590ea9cea8c9ea
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-03 10:43:52 +0530

    created a cart icon and dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,10 @@
 import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
 import { UserContext } from '../../context/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.util';
 import './navigation.styles.scss';
@@ -29,7 +33,10 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
