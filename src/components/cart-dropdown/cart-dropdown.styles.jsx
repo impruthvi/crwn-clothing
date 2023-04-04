12eb1c0a20c20a401108e86f88b1ab50c643aab9
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-04 14:45:26 +0530

    fixed the cart dropdown style

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -8,8 +8,8 @@ import {
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
-  width: 240px;
-  height: 340px;
+  width: 318px;
+  height: 377px;
   display: flex;
   flex-direction: column;
   padding: 20px;
@@ -32,8 +32,8 @@ export const EmptyMessage = styled.span`
 `;
 
 export const CartItems = styled.div`
-  height: 240px;
+  height: auto;
   display: flex;
   flex-direction: column;
-  overflow: scroll;
+  overflow-y: auto;
 `;
