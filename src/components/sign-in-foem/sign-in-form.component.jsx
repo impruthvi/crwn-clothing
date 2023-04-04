624f4a7bc914ff4aaaac59d4fb5c32b523e76d4d
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-04 10:41:50 +0530

    implemented style component for button

--- src/components/sign-in-foem/sign-in-form.component.jsx
+++ src/components/sign-in-foem/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
   signInAuthWithEmailAndPassword,
   signInWithGooglePopup,
 } from '../../utils/firebase/firebase.util';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
 
 import './sign-in.styles.scss';
@@ -79,7 +79,7 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type='button' buttonType="google" onClick={signInWithGoogle}>
+          <Button type='button' buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>
             Google Sign In
           </Button>
         </div>
