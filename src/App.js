645d952a6b0bc1a1aa12c7715f65b570d96e2236
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-06 17:45:35 +0530

    get current user using redux saga

--- src/App.js
+++ src/App.js
@@ -7,25 +7,13 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 import { useEffect } from 'react';
 import {
-  createUserDocumentFromAuth,
-  onAuthStateChangedListener,
+  getCurrentUser,
 } from './utils/firebase/firebase.util';
-import { setCurrentUser } from './store/user/user.action';
-import { useDispatch } from 'react-redux';
 
 const App = () => {
 
-  const dispatch = useDispatch();
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user));
   }, []);
 
   return (
