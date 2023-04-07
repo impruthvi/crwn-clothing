61e5d9ae97c582de70ec1ff70e2fad2526494cb6
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-07 12:20:31 +0530

    implemented user saga for sign In user

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -5,6 +5,8 @@ import { signInSuccess, signInFailed } from './user.action';
 import {
   createUserDocumentFromAuth,
   getCurrentUser,
+  signInAuthWithEmailAndPassword,
+  signInWithGooglePopup,
 } from '../../utils/firebase/firebase.util';
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
@@ -14,9 +16,29 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
       userAuth,
       additionalDetails
     );
-    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
     console.log(userSnapshot);
     console.log(userSnapshot.data());
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signInWithGoogle({ payload: { email, password } }) {
+  try {
+    const { user } = yield call(
+      signInAuthWithEmailAndPassword(email, password)
+    );
+    yield call(getSnapshotFromUserAuth, user);
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signInWithEmail() {
+  try {
+    const { user } = yield call(signInWithGooglePopup);
+    yield call(getSnapshotFromUserAuth, user);
   } catch (error) {
     yield put(signInFailed(error));
   }
@@ -26,10 +48,17 @@ export function* isUserAuthenticated() {
   try {
     const userAuth = yield call(getCurrentUser);
     if (!userAuth) return;
-    yield call(getSnapshotFromUserAuth, userAuth)
+    yield call(getSnapshotFromUserAuth, userAuth);
   } catch (error) {
     yield put(signInFailed(error));
   }
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(USER_ACTION_TYPE.GOOGLE_SIGN_IS_START, signInWithGoogle);
+}
+export function* onEmailSignInStart() {
+  yield takeLatest(USER_ACTION_TYPE.EMAIL_SIGN_IN_START, signInWithGoogle);
 }
 
 export function* onCheckUserSession() {
@@ -37,5 +66,9 @@ export function* onCheckUserSession() {
 }
 
 export function* userSagas() {
-  yield all([call(onCheckUserSession)]);
+  yield all([
+    call(onCheckUserSession),
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+  ]);
 }
