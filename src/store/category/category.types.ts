3774c6da213d5955789fa67ced4015976e84721f
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-16 12:28:16 +0530

    implemented typescript in the category redux

---
+++ src/store/category/category.types.ts
@@ -0,0 +1,23 @@
+export enum CATEGORY_ACTION_TYPES {
+  SET_CATEGORIES = 'category/SET_CATEGORIES',
+  FETCH_CATEGORIES_START = 'category/FETCH_CATEGORIES_START',
+  FETCH_CATEGORIES_SUCCESS = 'category/FETCH_CATEGORIES_SUCCESS',
+  FETCH_CATEGORIES_FAILED = 'category/FETCH_CATEGORIES_FAILED',
+}
+
+export type CategoryItem = {
+  id: number;
+  imageURL: string;
+  name: string;
+  price: number;
+};
+
+export type Category = {
+  title: string;
+  imageURL: string;
+  items: CategoryItem[];
+};
+
+export type CategoryMap = {
+  [key: string]: CategoryItem[];
+};
