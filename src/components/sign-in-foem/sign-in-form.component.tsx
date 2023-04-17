3bc946e408c00b2aa9fc428bf49366b77c9cf7de
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-04-17 15:41:03 +0530

    implemented typescript in forms

--- src/components/sign-in-foem/sign-in-form.component.tsx
+++ src/components/sign-in-foem/sign-in-form.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
@@ -8,6 +8,7 @@ import {
   emailSignInStart,
   googleSignInStart,
 } from '../../store/user/user.action';
+import { AuthErrorCodes, AuthError } from 'firebase/auth';
 
 const defaultFormFields = {
   email: '',
@@ -27,19 +28,19 @@ const SignInForm = () => {
     dispatch(googleSignInStart());
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
       dispatch(emailSignInStart(email, password));
       resetFormFields();
     } catch (error) {
-      switch (error.code) {
-        case 'auth/wrong-password':
+      switch ((error as AuthError).code) {
+        case AuthErrorCodes.INVALID_PASSWORD:
           alert('incorrect password for email');
           break;
 
-        case 'auth/user-not-found':
+        case AuthErrorCodes.USER_DELETED:
           alert('No user associated with this email');
           break;
         default:
@@ -49,7 +50,7 @@ const SignInForm = () => {
     }
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
   };
--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
 import { SignUpContainer } from './sign-up.styles';
 import { useDispatch } from 'react-redux';
 import { signUpStart } from '../../store/user/user.action';
-
+import { AuthErrorCodes , AuthError} from 'firebase/auth';
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -21,7 +21,7 @@ const SignUpForm = () => {
     setFormFields(defaultFormFields);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event:FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (password !== confirmPassword) {
@@ -33,7 +33,8 @@ const SignUpForm = () => {
       dispatch(signUpStart(email, password, displayName));
       resetFormFields();
     } catch (error) {
-      if (error.code === 'auth/email-already-in-use') {
+      
+      if ((error as AuthError).code === AuthErrorCodes.EMAIL_EXISTS) {
         alert("Can't create user, email already in use");
       } else {
         console.log('User creation encountered an error', error);
@@ -41,7 +42,7 @@ const SignUpForm = () => {
     }
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event:ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
   };
